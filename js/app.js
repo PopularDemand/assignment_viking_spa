a86602ea6a5567b575212baaf910d26e1308cb3c
Alexa <alexa.anderson@outlook.com>
2017-01-26 10:51:27 -0600

    Store products in json

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-var spa = angular.module('spa', []);
+var spa = angular.module('spa', ['ui.router']);
+
+spa.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/products');
+
+  $stateProvider
+    .state('products', {
+      url: '/products',
+      templateUrl: 'js/templates/productsIndex.html',
+      controller: 'ProductsCtrl'
+    })
+})
---
+++ js/controllers/productsController.js
@@ -0,0 +1,3 @@
+spa.controller('ProductsCtrl', ['$scope', function($scope) {
+  $scope.words = 'these are words';
+}])
